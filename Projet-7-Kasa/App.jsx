13a86a59b2fed434c960449f24e5ff43f2fd4f12
sprisca <priscasaina5@gmail.com>
2024-10-18 10:36:24 +0200

    supression

--- Projet-7-Kasa/App.jsx
+++ Projet-7-Kasa/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom' // CREATION DE LA NAVIGATION
 import Header from './components/header/header.jsx'
 import Footer from './components/footer/footer.jsx'
 import Home from './pages/home/home.jsx'
 import About from './pages/about/about.jsx'
 import Logement from './pages/logement/logement.jsx'
 import Error from './pages/error/error.jsx'
+
+// jonglage entre les composants
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter> 
     <div>
     <Header />
     <Routes>
       <Route path ='/' element={<Home />} />
       <Route path ='/about' element={<About />} />
-      <Route path ='/logement/:id' element={<Logement />} />
+      <Route path ='/logement/:id' element={<Logement />} /> 
       <Route path ='*' element={<Error />} />
     </Routes>
     </div>
--- Projet-7-Kasa/src/components/header/header.jsx
+++ Projet-7-Kasa/src/components/header/header.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import logoHeader from '../../assets/images/logo-header.png'
+import logoHeader from '../../assets/images/logo-header.png' // importation de l'image
 import './header.scss'
-
+// creation des lien de navigation
 function Header() {
   return (
     <div className='navbar'>
      <img src= {logoHeader} alt="logo Kasa" />
      <ul>
             <li>
-              <NavLink to= "/">Accueil</NavLink></li>
+              <NavLink to= "/">Accueil</NavLink></li> 
             <li>
               <NavLink to= "/about">A Propos</NavLink></li>
         </ul>
